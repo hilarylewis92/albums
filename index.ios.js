cd955e63cad65bca80f55d3da7209668a7dffbf1
Hilary Lewis <hilarylewis92@gmail.com>
2017-05-22 10:45:26 -0600

    Set up components

--- index.ios.js
+++ index.ios.js
@@ -2,12 +2,16 @@
 
 // import library to help create a Component
 import React from 'react'
-import { AppRegistry } from 'react-native'
-import Header from './src/components/header'
+import { AppRegistry, View } from 'react-native'
+import Header from './src/components/Header'
+import AlbumList from './src/components/AlbumList'
 
 // create Component
 const App = () => (
-  <Header />
+  <View>
+    <Header headerText={'Albums'}/>
+    <AlbumList />
+  </View>
 )
 
 //render to device
